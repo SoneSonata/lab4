64ac93f8e1f6e38411ec9c2d5a360c58bc9af9e5
Ben Smith <bengsmith18@gmail.com>
2023-02-28 10:29:14 -0800

    Added main js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let mybutton = document.querySelector('.createbutton');
 mybutton.addEventListener('click', renderImg);
-const pics = []
+const baseURL = 'images/';
+const pics = ['pic1.png', 'pic2.png', 'pic3.png', 'pic4.jpg', 'pic5.jpg', 'pic6.jpg', 'pic7.jpg', 'pic8.jpg', 'pic9.jpg'];
 let index = 0;
 
 function getNumImg(){
@@ -13,11 +14,38 @@ function getNumImg(){
     return numImgs;
 }
 
-function renderImg() {
+function makeImg() {
     const imgNum = getNumImg();
     const tempHolder = document.createDocumentFragment();
 
     for (let i = 0; i <= imgNum; i++) {
+        let pic = document.createElement(img);
+        pic.src = baseURL + pics[index];
+        let txt = document.createTextNode(i);
+        pic.className = "picture";
+        document.body.appendChild(pic);
+        index++
+
+        if(index > pics.length){
+            index = 0;
+        }
+
+        pic.appendChild(txt);
+        tempHolder.appendChild(pic);
 
     }
+    return tempHolder;
+}
+
+function renderImg(e) {
+    const containerName = e.target.dataset.holder;
+    const container = document.querySelector(containerName);
+    const genpic = makeImg();
+
+    if (genpic.children.length === 0) {
+        container.innerHTML = "Try Again. Please type a positive whole number";
+        } else {
+        container.innerHTML = "";
+        container.appendChild(genpic);
+        }
 }
